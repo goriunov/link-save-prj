6c9bd79140111b86183283533b81d09d3b78838f
goriunovd <gotiunovd@gmail.com>
2016-09-27 18:16:14 +1300

    Stylinkg loading page

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -3,7 +3,7 @@ import './polyfills.ts';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
-import { AppModule } from '../../../../Uneversal/universal-starter/src/app/app/';
+import { AppModule } from './app';
 
 if (environment.production) {
   enableProdMode();
